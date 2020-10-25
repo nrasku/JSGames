5e8b34a9ec45e7ede72331db929927812a4aac7b
Niko Rasku <niko.rasku@gmail.com>
2020-10-25 14:09:33 +0200

    Fixed looping background

--- backdrop/background.js
+++ backdrop/background.js
@@ -1,6 +1,6 @@
 CANVAS_WIDTH = 600;
 CANVAS_HEIGTH = 380;
-BACKGROUND_SPEED = -0.3;
+BACKGROUND_SPEED = -0.2;
 
 function Background(item) {
     this.x = item.x;
@@ -13,15 +13,20 @@ function Background(item) {
 }
 
 Background.prototype.draw = function() {
+    // Draw currently visible background
     ctx.drawImage(this.image, 
         this.x, 
         this.y, this.width, this.heigth);
+    // Draw second looping background at the edge of visible background
+    ctx.drawImage(this.image, 
+          this.x + this.width, 
+          this.y, this.width, this.heigth);
     
 }
 
 Background.prototype.update = function() {
     this.x += this.speed;
-    if (this.x <= -(this.width - CANVAS_WIDTH)) {
+    if (this.x <= -(this.width)) {
         this.x = 0;
     }
 }
